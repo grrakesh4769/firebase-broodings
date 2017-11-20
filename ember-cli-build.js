f5224ea69133f62a4dddd434462ceb7c0410f60a
rakesh.gr <rakesh.gr@zohocorp.com>
2017-11-20 23:48:51 +0530

    add bootstrap

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -20,6 +20,12 @@ module.exports = function(defaults) {
   // modules that you would like to import into your application
   // please specify an object with the list of modules as keys
   // along with the exports of each module as its value.
+  app.import('vendor/js/bootstrap.bundle.js');
+
+  app.import('vendor/css/bootstrap.css');
+  app.import('vendor/css/bootstrap-grid.css');
+  app.import('vendor/css/bootstrap-reboot.css');
+  app.import('vendor/css/agency.css');
 
   return app.toTree();
 };
