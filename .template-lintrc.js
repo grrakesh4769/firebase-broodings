44881fc7ddaa5b4dfce3a911c577909964445988
grrakesh4769 <grrakesh4769@gmail.com>
2018-09-06 20:48:44 +0530

    template lint

---
+++ .template-lintrc.js
@@ -0,0 +1,12 @@
+//ignorei18n_start
+module.exports = {
+  //extends: 'recommended',
+
+  rules: {
+    'bare-strings': true,
+    'html-comments': true,
+    'link-rel-noopener': true,
+    'simple-unless': true
+  }
+}
+//ignorei18n_end
