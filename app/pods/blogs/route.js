ef44c6788ecd2570aa851a6cf406c5bf1340a0d2
grrakesh4769 <grrakesh4769@gmail.com>
2019-02-16 10:37:44 +0530

    change model name

--- app/pods/blogs/route.js
+++ app/pods/blogs/route.js
@@ -1,4 +1,10 @@
 import Route from '@ember/routing/route';
+import Ember from 'ember';
 
 export default Route.extend({
+  model(){
+    return Ember.RSVP.hash({
+      BLOGS: this.get('store').findAll('post')
+    })
+  }
 });
