e2bd4241e1da18e865323b34971ff6e174d726b2
grrakesh4769 <grrakesh4769@gmail.com>
2019-02-16 12:12:59 +0530

    create post complete

--- app/models/post.js
+++ app/models/post.js
@@ -2,5 +2,9 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   title: DS.attr(),
-  content: DS.attr()
+  content: DS.attr(),
+  post_created_at: DS.attr(),
+  post_last_modified_at: DS.attr({
+    defaultValue: -1
+  })
 });
--- app/pods/blogs/new/route.js
+++ app/pods/blogs/new/route.js
@@ -2,9 +2,23 @@ import Route from '@ember/routing/route';
 import Ember from 'ember';
 
 export default Route.extend({
-  model(){
+  model() {
     return Ember.RSVP.hash({
       NEW_POST: this.get('store').createRecord('post') //No I18N
     })
+  },
+  clearTempData: Ember.on('deactivate', function() {
+    this.controller.get('model.NEW_POST').rollbackAttributes();
+  }),
+  actions: {
+    createPost: function () {
+      let self = this;
+      if (this.controller.get('model.NEW_POST.hasDirtyAttributes')) {
+        this.controller.set('model.NEW_POST.post_created_at', moment().valueOf());
+        this.controller.get('model.NEW_POST').save().then(function() {
+          self.transitionTo('blogs.list');
+        })
+      }
+    }
   }
 });
